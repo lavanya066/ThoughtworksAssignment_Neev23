c4e67f17698a84ebfb654dcfb76149ade34d30f6
lavanya066 <lavanyark06@gmail.com>
2022-09-06 11:25:56 +0530

    todo list add and edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import DisplayList from './components/DisplayList';
 
 function App() {
+  const [todoName,setTodoName]=useState("")
+  const [todoList,setList]=useState([])
+  const [update,setUpdate]=useState(false)
+  const [name,setName]=useState("")
+  const [key,setKey]=useState("")
+  useEffect(()=>{
+<DisplayList list={todoList} deleteItem={deleteItem} updateItem={updateItem}></DisplayList>
+  },[])
+  function changeInp(e){
+    setTodoName(e.target.value)
+  }
+  function addList(e){
+    if(update){
+      console.log(name)
+      setList(todoList.map((v)=>{
+        var temp = Object.assign({}, v);
+        if(temp.name==name){
+          temp.name=todoName
+        }
+        return temp
+      }))
+    //   var index=todoList.indexOf({name:name,key:key});
+    //   console.log(index)
+    // todoList[index].name=todoName
+    setUpdate(false)
+    setName("")
+    
+    }
+    else{
+      setList([...todoList,{name:todoName , key:todoList.length}])
+    }
+    
+    
+    setTodoName("")
+  }
+  function deleteItem(val){
+    setList(todoList.filter(function(value){
+      return value.name!=val
+  }))
+  
+  }
+  function updateItem(val,key){
+    console.log(todoList)
+    setName(val)
+    setUpdate(true)
+    setKey(key)
+    
+    setTodoName("")
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <label  >Enter todo:</label>
+      <input type="text" id="totdoinp" onChange={changeInp} value={todoName}></input>
+      <button onClick={addList}> Add</button>
+      <DisplayList list={todoList} deleteItem={deleteItem} updateItem={updateItem}></DisplayList>
     </div>
   );
 }
---
+++ src/components/DisplayList.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Display from './Display.css';
+function DisplayList(props) {
+    //var l=props.list
+    const [l,setl]=useState([...props.list])
+    
+    const [form,setForm]=useState("")
+    
+    useEffect(()=>{
+        setForm(
+            
+                <div>
+             {
+                 props.list.map((val)=>{
+                     return(
+                         <div className='list'>
+                             <h3>{val.name}</h3>
+                             
+                             <div>
+                                 <DeleteIcon className='delete' onClick={(e)=>{
+                                    
+                                     
+                                    props.deleteItem(val.name)
+                                     
+                                     
+                                 }
+                                     
+                                 }></DeleteIcon>
+                             <EditIcon className='edit' onClick={(e)=>{
+                                props.updateItem(val.name,val.key)
+                             }}></EditIcon>
+                                 </div>
+                             
+                             </div>
+                     )
+                 })
+                 
+             }
+             </div>
+         
+             )
+           
+        
+        
+        
+    },[props.list])
+
+  return (
+    
+       <div>
+        {form}
+
+       </div> 
+
+    
+  
+  )
+}
+
+export default DisplayList
